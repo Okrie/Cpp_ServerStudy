2efbbdaed62938e1e1254035b79efb4e66a2e6df
OK <okrie@test>
2021-03-26 12:01:22 +0900

    sample winsock
    github.com/LimSungMin/winsock-server-client/blob/master/winsock_sample.cpp
    
    참고

---
+++ fourth_winsock/winsock_Sample.cpp
@@ -0,0 +1,43 @@
+// 콘솔 응용 프로그램의 진입점을 정의
+
+#include "stdafx.h"
+#include <iostream>
+#include <process.h>
+
+#include "cSocket.h"
+#include <Windows.h>
+
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8000
+
+using namespace std;
+
+int main()
+{
+    // 소켓 객체 생성
+    cSocket socket;
+
+    int test;
+    cout << "server 1 / client 2 : ";
+    cin >> test;
+
+    // echo 서버로 시작
+    if(test == 1)
+    {
+        socket.InitSocket();
+        socket.BindAndListen(SERVER_PORT);
+        socket.StartServer();
+    }
+    // echo 클라이언트로 시작
+    else if(test == 2)
+    {
+        socket.InitSocket();
+        socket.Connect(SERVER_IP, SERVER_PORT);
+    }
+    else{
+        cout << "type 1 or 2" << endl;
+        return 0;
+    }
+
+    return 0;
+}
